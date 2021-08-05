100cdf26158a64011b6a755099984e97e1fc1e39
Kayleighht <79431886+Kayleighht@users.noreply.github.com>
2021-08-05 09:10:40 -0400

    Add files via upload

---
+++ Scripts/2_MaxDBH_Data_Calculations.R
@@ -0,0 +1,61 @@
+#### Load Packages
+Packages.2 <- c("tidyr")
+lapply(Packages.2, library, character.only = TRUE)
+
+#For missing Maximum DBH values I followed the same procedure as the original public tree dataset
+#Any tree species with at least 10 trees in my dataset we compiled and the 95 percentile was calculated based on measured DBH values to create a maximum value
+#Below, I calculate maximum DBH values for the necessary tree species
+
+#read in and format datafile
+metacsv<- read.csv("Metadata_GS_CU.csv")
+site.sp.table<- table(metacsv$Species.Code, metacsv$Subsite.Code)
+site.sp.table<- as.data.frame(site.sp.table)
+site.sp.table
+
+##formatting data for calculations
+#here I am using an OUTPUT file that I created in script one. This file is sourced in the folder "output" rather than "input"
+total.sp.table<-read.csv("allgs.matrix.csv")
+sp.tot.ab<- as.data.frame(t(total.sp.table))
+colnames(sp.tot.ab)<- c("Inst", "Park", "Private", "Row")
+sp.tot.ab<-sp.tot.ab[-1,]
+#convert columns to numerics rather than factor
+i <- c(1:4)      
+sp.tot.ab[ , i] <- apply(sp.tot.ab[ , i], 2,        
+                         function(x) as.numeric(as.character(x)))
+sapply(sp.tot.ab, class)
+#summing across rows
+sp.tot.ab<-sp.tot.ab %>% mutate(sumrow= Inst + Park + Private + Row)
+
+#subset for taxus canadensis
+taxus<-filter(metacsv, Species.Code == "TACA")
+taxus
+#calculating the 95 percentile based on DBH data on species 
+quantile(taxus$DBH..2020.., 0.95)
+
+#subset acer palmatum 
+palmatum<- filter(metacsv, Species.Code == "ACPA")
+quantile(palmatum$DBH..2020.., 0.95)
+
+#subset caragana aborescens 
+caragana<- filter(metacsv, Species.Code == "CAAB")
+quantile(caragana$DBH..2020.., 0.95)
+
+#subset smoketree
+smoketree<- filter(metacsv, Species.Code == "CTCO")
+quantile(smoketree$DBH..2020.., 0.95)
+
+#subset euonymous alatus
+euonal<- filter(metacsv, Species.Code == "EUAL")
+quantile(euonal$DBH..2020.., 0.95)
+
+#subset lonicera xylosteum
+loxy<- filter(metacsv, Species.Code == "LOXY")
+quantile(loxy$DBH..2020.., 0.95)
+
+#subset viburnum letago
+nannyberry<- filter(metacsv, Species.Code == "VILE")
+quantile(nannyberry$DBH..2020.., 0.95)
+
+
+
+
---
+++ Scripts/6_Mixed_Models.R
@@ -0,0 +1,161 @@
+#### LINEAR MIXED MODELS #### 
+
+#load packages
+Packages.6 <- c("broom.mixed", "ggplot2", "lme4", "ggpubr", "lmerTest", "sjPlot", "car", "effects", "multcomp", "dplyr", "glmmTMB")
+lapply(Packages.6, library, character.only = TRUE)
+
+#read in metadata file
+LMM.Meta<- read.csv("LMM_META_FINAL.csv")
+#convert species richness values to numeric (rather than factor) for models
+LMM.Meta$SR<-as.numeric(LMM.Meta$SR)
+class(LMM.Meta$SR)
+
+#### GREEN SPACE TYPE AND SPECIES RICHNESS #### 
+
+### runnning a linear mixed model to test the effect of green space type on species richness, including logged sample area as a covariate 
+model.gs.sr<- glmer(SR ~ GS.Type + log(Area) + (1|Site.Code), data= LMM.Meta, family = "negative.binomial"(theta = 1))
+print(model.gs.sr)
+summary(model.gs.sr)
+
+#model diagnostics
+plot(model.gs.sr, which = 1)
+shapiro.test(resid(model.gs.sr))
+hist(residuals(model.gs.sr))
+qqnorm(residuals(model.gs.sr))
+qqline(residuals(model.gs.sr))
+
+#check effect of green space type on species richness using model outputs
+Anova(model.gs.sr)
+#push out model summary as csv file for plotting
+tbl.1 <- broom::tidy(model.gs.sr)
+tbl.1
+write.csv(tbl.1, "/home/kayleighhutttaylor/Ch. 1 Thesis Analysis/Output/Sr.Model.Sum.csv")
+
+#tukey test to check pairwise comparisons of species richness of each green space type
+tukey.test<-Pairwise.sr<-glht(model.gs.sr, mcp(GS.Type = "Tukey")) 
+#summarizing pairwise differences across green space types
+summary(Pairwise.sr) 
+
+#### GREEN SPACE TYPE AND TREE ABUNDANCE ####
+### runnning a linear mixed model to test the effect of green space type on tree abundance, including logged sample area as a covariate 
+model.gs.log<- lmer(log(Abund) ~ GS.Type + log(Area) + (1|Site.Code), data = LMM.Meta)
+plot(model.gs.log)
+
+#model diagnostics
+shapiro.test(resid(model.gs.log))
+qqnorm(residuals(model.gs.log))
+qqline(residuals(model.gs.log))
+hist(residuals(model.gs.log))
+
+##summary
+summary(model.gs.log)
+##Anova to test the effect of green space type on tree abundance
+Anova(model.gs.log)
+
+#push out model summary as csv file for plotting
+tbl.2 <- broom::tidy(model.gs.log)
+write.csv(tbl.2, "/home/kayleighhutttaylor/Ch. 1 Thesis Analysis/Output/AB.Model.Sum.csv")
+
+#pairwise comparisons
+Pairwise.ab<-glht(model.gs.log, mcp(GS.Type = "Tukey")) 
+summary(Pairwise.ab)
+
+#### GS TYPE AND EVENNESS (PIE) ####
+### runnning a linear mixed model to test the effect of green space type on species evenness (PIE), including logged sample area as a covariate 
+model.gs.ev <- lmer(PIE ~ GS.Type + log(Area) + (1|Site.Code), data= LMM.Meta)
+summary(model.gs.ev)
+
+#Model Diagnostics
+qqnorm(residuals(model.gs.ev))
+qqline(residuals(model.gs.ev))
+hist(residuals(model.gs.ev))
+plot(residuals(model.gs.ev))
+shapiro.test(resid(model.gs.ev))
+
+##Anova to test the effect of green space type on species evenness
+Anova(model.gs.ev)
+#push out model summary as csv file for plotting
+tbl.3 <- broom::tidy(model.gs.ev)
+write.csv(tbl.3, "/home/kayleighhutttaylor/Ch. 1 Thesis Analysis/Output/PIE.Model.Sum.csv")
+
+#pairwise comparison
+Pairwise.ev<-glht(model.gs.ev, mcp(GS.Type = "Tukey")) 
+summary(Pairwise.ev) 
+
+#### PLOTTING MODEL ESTIMATES AND ERRORS IN ONE FIGURE ####
+
+#read in parcel-scale means csv files (tree abundance, species richness and PIE)
+#each of these files contain calculated mean values and standard error for parcel-scale (the scale of management/an individual park or place of worship)
+ab.means<- read.csv("ab.site.means.csv")
+pie.means<- read.csv("pie.site.means.csv")
+sr.means<- read.csv("sr.site.means.csv")
+
+##read in files with model estimates from model run above for plotting
+est.ab<-read.csv("AB.ModelEst.csv")
+est.sr<- read.csv("SR.ModelEst.csv")
+est.pie<- read.csv("PIE.ModelEst.csv")
+
+#assigning a jitter value for all following plots
+jitter <- position_jitter(width = 0.2, height = 0.01)
+
+#### TREE ABUNDANCE PLOT ####
+
+#assigning axis labels for plot
+ab.labs<- labs(x= "Green Space Type", y= "Tree Abundance")
+#logging raw abundance values and adding as a new column since model used logged transformed data
+LMM.Meta$loggedabund<- log(LMM.Meta$Abund)
+LMM.Meta
+
+#adding column of logged abundance means
+ab.means$logged.ab.site<-log(ab.means$ab.site)
+##subsetting only parks and institutional values to show parcel scale values (one park, one school etc.)
+parcel.ab.means<- ab.means[-c(24:221),]
+##adding a column that shows green space type (either inst or park)
+parcel.ab.means$GS.Type<- c("Inst","Inst","Inst","Inst","Inst","Inst","Inst","Inst","Inst","Inst","Inst","Inst","Inst","Inst","Inst","Inst","Park","Park","Park","Park","Park","Park","Park")
+
+##PLOT
+model.ab.plot<-ggplot(LMM.Meta, aes(GS.Type, loggedabund)) 
+final.ab<-model.ab.plot + theme_minimal() + geom_point(data= parcel.ab.means, aes(x= GS.Type, y= logged.ab.site), colour= "black", position= jitter, size= 3) + geom_jitter(width= 0.4, height = 0.4, alpha= 0.5, colour= "grey", size= 3) +
+  ab.labs + geom_errorbar(data = est.ab, aes(x= GS.Type, y= Estimated.Mean, ymin= Estimated.Mean-SE, ymax= Estimated.Mean+SE), color= "red", width= 0.08, size= 1.5) + 
+  theme(axis.text.x = element_blank(), axis.title.y = element_text(face = "bold", size = 16))
+final.ab
+
+#### SPECIES RICHNESS PLOT ####
+
+#assigning axis labels
+sr.labs<- labs(x= "", y= "Richness")
+
+#subsetting parcel scale species richness means for parks and institutions
+sr.parcel.means<- sr.means[-c(24:221),]
+#adding GS type to parcel scale means for plotting
+sr.parcel.means$GS.Type<- c("Inst","Inst","Inst","Inst","Inst","Inst","Inst","Inst","Inst","Inst","Inst","Inst","Inst","Inst","Inst","Inst","Park","Park","Park","Park","Park","Park","Park")
+
+#plotting
+model.sr.plot <- ggplot(LMM.Meta, aes(GS.Type, SR))
+final.sr<-model.sr.plot + theme_minimal() + geom_point(data= sr.parcel.means, aes(x= GS.Type, y= sr.site), colour= "black", position = jitter, size= 3) + geom_jitter(width= 0.4, height = 0.4, alpha= 0.5, colour= "grey", size= 3) + sr.labs +
+  geom_errorbar(data = est.sr, aes(x= GS.Type, y= Estimated.Mean, ymin= Estimated.Mean - SE, ymax= Estimated.Mean + SE), size= 1.5, color= "red", width= 0.08) + theme(
+    axis.text.x = element_blank(), axis.title.y = element_text(face = "bold", size = 16))
+final.sr
+
+#### SPECIES EVENNESS (PIE) PLOT ####
+
+#assigning axis labels
+pie.labs<- labs(x= "Green Space Type", y= "Evenness")
+
+#subsetting parcel scale species evenness (PIE) means for parks and institutions
+pie.parcel.means<- pie.means[-c(24:221),]
+#adding GS type to parcel scale means for plotting
+pie.parcel.means$GS.Type<- c("Inst","Inst","Inst","Inst","Inst","Inst","Inst","Inst","Inst","Inst","Inst","Inst","Inst","Inst","Inst","Inst","Park","Park","Park","Park","Park","Park","Park")
+
+#plotting
+model.pie.plot<-ggplot(LMM.Meta, aes(GS.Type,PIE)) 
+final.ev<-model.pie.plot + geom_errorbar(data = est.pie, mapping = aes(x = GS.Type, y = Estimated.Mean, ymin = Estimated.Mean - SE, ymax = Estimated.Mean + SE), size=1.5, color="red", width=.08) + 
+  theme_minimal() + geom_point(data= pie.parcel.means, mapping = aes(x= GS.Type, y= PIE.site), colour= "black", position= jitter, size= 3) + geom_jitter(width= 0.2, height = 0.01, alpha= 0.5, colour= "grey", size= 3) + pie.labs +
+  theme(axis.text.x = element_text(size= 16, colour= "black", face= "bold"), axis.title.y = element_text(face = "bold", size = 16))
+final.ev
+
+###WRAPPING INTO ONE FIGURE
+model.figure<- ggarrange(final.ab, final.sr, final.ev,
+                         nrow= 3, ncol= 1,
+                         labels = c("A", "B", "C"), font.label =  list(size =9), hjust = -1, vjust = 1.5)
+model.figure
